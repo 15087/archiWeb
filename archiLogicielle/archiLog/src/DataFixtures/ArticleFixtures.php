f27f6ec6757c8d62e8b76510f33f93a4eeecf827
Hana-Jabbour <36258812+Hana-Jabbour@users.noreply.github.com>
2020-12-23 13:42:55 +0100

    Fixtures
    Créations de fausses informations (fixtures)

--- archiLogicielle/archiLog/src/DataFixtures/ArticleFixtures.php
+++ archiLogicielle/archiLog/src/DataFixtures/ArticleFixtures.php
@@ -5,16 +5,34 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <> 10; $i++){
+        $faker = \Faker\Factory::create('fr_FR');
+        
+        //Création de 3 fake catégories
+        for($i = 1; $i <= 3; $i++){
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+        }
+        // Création faux articles
+
+        
+        $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+        
+
+        for($j = 1; $j <= mt_rand(8,10); $j++){
             $article = new Article();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("<p> Contenu de l'article n°$i</p>")
-                    ->setImage("http://placehold.it/350x150");
+            $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCategory($category);
             
             $manager->persist($article);
         }
